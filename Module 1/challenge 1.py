b07b6d0573d017dacc623be92e12d27de431d013
Shoikot <96073081+shykat1@users.noreply.github.com>
2022-03-20 15:07:29 +0600

    remove even integers from list

---
+++ Module 1/challenge 1.py
@@ -0,0 +1,10 @@
+check_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+for i in check_list:
+    if (i % 2) == 0:
+        pass
+    else:
+        check_list.remove(i)
+
+print(11%2)
+print(check_list)
